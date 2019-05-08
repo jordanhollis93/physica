83453fde0a91799be514c4381977a022f5ba1eaf
jordan hollis <jaycoradio@gmail.com>
2019-05-08 13:38:33 -0700

    update

---
+++ server.js
@@ -0,0 +1,20 @@
+// Wrap every letter in a span
+$('.ml10 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: function(el, i) {
+      return 45 * i;
+    }
+  }).add({
+    targets: '.ml10',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
